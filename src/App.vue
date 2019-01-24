16f85ba115f50628164f42029dcf9a6e620e0681
Juan Sifontez <juansfontz@gmail.com>
2019-01-24 18:14:11 -0400

    feat: Add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/layout/Footer.vue'
 
 export default {
   name: 'app',
+  components: { Footer }
 }
 </script>
 
 <style>
+html, body{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body{
+  margin-top: -16px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   color: #2c3e50;
+  height: 100%;
 }
 </style>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <p>
+        <strong>Powered by <a href="https://quotesondesign.com/api-v4-0/" target="_blank">Quotes on Desing</a></strong>
+    </p>
+    <p>
+      <strong>
+        Made with <span role="img" aria-label="heart">❤ by </span> <a href="https://twitter.com/Jsifontez_" target="_blank" rel="noopener noreferrer">@Jsifontez</a>
+      </strong>
+    </p>
+    <p>
+      <strong>
+        Commits are welcome on <a href="https://github.com/100DaysOfVue/random-quote-machine" target="_blank" rel="noopener noreferrer">Github</a>
+      </strong>
+    </p>
+  </div>
+</template >
