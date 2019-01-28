d4b933bbe2a51bd1ac751bc428bc38d4c06088a6
Juan Sifontez <juansfontz@gmail.com>
2019-01-27 21:01:12 -0400

    feat: Add styles to quote component

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="quote">
-    <h1 v-if="quote" class="quote--up">Random Quote Machine in other div</h1>
-    <div class="quote--down">
-      <h1 class="quote--down__quote" v-if="quote == false">Random Quote machine</h1>
-      <button :class="[quote ? 'btn': 'btn--quote']"
-        @click="toogleQuote"
-      >
-        Get your Quote
-      </button>
+  <div class="main">
+    <h1 v-if="quoteState" class="main__title--up">Random Quote Machine in other div</h1>
+    <div class="quote__container">
+      <h1 class="main__title--down" v-if="!quoteState">Random Quote machine</h1>
+      <div v-else class="quote">
+        <p> {{ quote }} </p>
+        <p><strong>- {{ quoteAuthor }} -</strong></p>
+      </div>
+      <div class="btn__container" :class="[quoteState ? 'btn__container--right' : '']">
+        <button :class="[quoteState ? 'btn': 'btn--quote']"
+          @click="toogleQuoteStatus"
+        >
+          Get your Quote
+        </button>
+        <button v-if="quoteState" class="btn btn__twitter"></button>
+      </div>
     </div>
   </div>
 </template>
@@ -19,30 +26,34 @@ export default {
   name: 'quote',
   data () {
     return {
-      quote: false
+      quoteState: false,
+      quote: '',
+      quoteAuthor: ''
     }
   },
   methods: {
-    toogleQuote () {
-      this.quote = !this.quote
-
+    toogleQuoteStatus (ev) {
+      this.getQuote()
+      this.quoteState = true
+    },
+    getQuote () {
       axios.get(`https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=1&cat=famous`,
         {
           headers: { 'X-RapidAPI-Key': '25ed2d9f1emshd52da26ac7daa32p1fb264jsn803f1bfb8c17' }
         }
       )
-        .then(res => console.log(res))
+        .then(res => {
+          this.quote = res.data[0].quote
+          this.quoteAuthor = res.data[0].author
+        })
         .catch(e => console.log(e))
-    },
-    getQuote (aasd) {
-      console.log(aasd)
     }
   }
 }
 </script>
 
 <style>
-.quote {
+.main {
   min-height: calc(100vh - 82px);
   text-align: center;
   display: grid;
@@ -50,31 +61,64 @@ export default {
   grid-gap: 5px;
 }
 
-.quote--up{
+.main__title--up{
   grid-row: 1 / 2;
 }
 
-.quote--down {
+.quote__container {
+  position: relative;
+  padding: .4em;
   grid-row: 2;
-  height: 300px;
-  width: 80%;
+  height: 150px;
+  width: 50%;
   justify-self: center;
   align-self: center;
   background-color: mistyrose;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+}
+
+.quote{
+  width: 90%;
+  height: 25vh;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.quote > p {
+  margin: 0;
+}
+
+.btn__container{
+  border: 1px solid blue;
+  position: absolute;
+  bottom: .4em;
+}
+
+.btn__container--right {
+  right: .4em;
 }
 
 .btn {
   width: 110px;
   height: 40px;
+  vertical-align: top;
 }
 
 .btn--quote {
   width: 181px;
   height: 50px;
   font-size: 1.5em;
+}
+
+.btn__twitter{
+  width: 40px;
+  background-color: grey;
+  border-radius: 50%;
+  border: none;
+  margin-left: 10px;
 }
 </style>
